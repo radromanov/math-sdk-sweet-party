6505491f4b1dc5926c8e64128cab1b67b3c4bbe0
r.romanov <r.romanov@wienergames.com>
2026-03-26 08:11:17 +0200

    fix: capped scatter count at max 7 scatters

--- games/sweet_party/game_executables.py
+++ games/sweet_party/game_executables.py
@@ -1,10 +1,32 @@
 from game_calculations import GameCalculations
 from src.calculations.cluster import Cluster
-from src.events.events import update_freespin_event
+from src.events.events import update_freespin_event, fs_trigger_event
 
 
 class GameExecutables(GameCalculations):
     """Game dependent grouped functions."""
+
+    def _capped_scatter_count(self, scatter_key: str = "scatter") -> int:
+        """Return scatter count capped to max key in freespin_triggers for current gametype."""
+        raw_count = self.count_special_symbols(scatter_key)
+        max_trigger = max(self.config.freespin_triggers[self.gametype].keys())
+        return min(raw_count, max_trigger)
+
+    def update_freespin_amount(self, scatter_key: str = "scatter") -> None:
+        """Set initial number of spins, capping scatter count to max trigger key."""
+        capped = self._capped_scatter_count(scatter_key)
+        self.tot_fs = self.config.freespin_triggers[self.gametype][capped]
+        if self.gametype == self.config.basegame_type:
+            basegame_trigger, freegame_trigger = True, False
+        else:
+            basegame_trigger, freegame_trigger = False, True
+        fs_trigger_event(self, basegame_trigger=basegame_trigger, freegame_trigger=freegame_trigger)
+
+    def update_fs_retrigger_amt(self, scatter_key: str = "scatter") -> None:
+        """Update total freespin amount on retrigger, capping scatter count."""
+        capped = self._capped_scatter_count(scatter_key)
+        self.tot_fs += self.config.freespin_triggers[self.gametype][capped]
+        fs_trigger_event(self, freegame_trigger=True, basegame_trigger=False)
 
     def get_clusters_update_wins(self) -> None:
         """Find clusters on board and update win manager."""
