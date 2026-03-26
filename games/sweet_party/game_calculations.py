400ec6a28b6630877cdb2fd70296c70e9156dfe6
r.romanov <r.romanov@wienergames.com>
2026-03-26 07:43:59 +0200

    refactor: rewrite game calcs

--- games/sweet_party/game_calculations.py
+++ games/sweet_party/game_calculations.py
@@ -1,68 +1,53 @@
 from src.executables.executables import Executables
 from src.calculations.cluster import Cluster
-from src.calculations.board import Board
-from src.config.config import Config
+from src.calculations.symbol import Symbol
+from game_config import GameConfig
 
 
 class GameCalculations(Executables):
     """
-    This function will override the evaluate_clusters() function in cluster.py
-    This is to account for the grid multiplier in winning positions.
+    Game-specific cluster evaluation.
+    Caps cluster sizes at max_cluster_pay_size so oversized clusters
+    still pay at the highest paytable tier.
     """
 
-    # Override cluster evaluation functions to include grid position multipliers
-    def evaluate_clusters_with_grid(
-        self,
-        config: Config,
-        board: Board,
+    @staticmethod
+    def evaluate_clusters_capped(
+        config: GameConfig,
+        board: list[list[Symbol]],
         clusters: dict,
-        pos_mult_grid: list,
         global_multiplier: int = 1,
         return_data: dict = {"totalWin": 0, "wins": []},
-    ) -> type:
-        """
-        Determine payout amount from cluster, including symbol multiplier and global multiplier value.
-        Game specific function which takes into account position multipliers.
-        """
-        exploding_symbols = []
-        total_win = 0
+    ) -> tuple:
+        """Evaluate clusters with size capped at config.max_cluster_pay_size."""
+        max_size: int = config.max_cluster_pay_size
+        total_win = 0.0
         for sym in clusters:
             for cluster in clusters[sym]:
-                syms_in_cluster = len(cluster)
-                if (syms_in_cluster, sym) in config.paytable:
-                    board_mult = 0
-                    for positions in cluster:
-                        board_mult += pos_mult_grid[positions[0]][positions[1]]
-                    board_mult = max(board_mult, 1)
-                    sym_win = config.paytable[(syms_in_cluster, sym)]
-                    symwin_mult = sym_win * board_mult * global_multiplier
-                    total_win += symwin_mult
+                actual_size = len(cluster)
+                pay_size = min(actual_size, max_size)
+                if (pay_size, sym) in config.paytable:
+                    sym_win = config.paytable[(pay_size, sym)]
+                    cluster_win = sym_win * global_multiplier
+                    total_win += cluster_win
                     json_positions = [{"reel": p[0], "row": p[1]} for p in cluster]
-
                     central_pos = Cluster.get_central_cluster_position(json_positions)
-                    return_data["wins"] += [
+                    return_data["wins"].append(
                         {
                             "symbol": sym,
-                            "clusterSize": syms_in_cluster,
-                            "win": symwin_mult,
+                            "clusterSize": actual_size,
+                            "win": cluster_win,
                             "positions": json_positions,
                             "meta": {
                                 "globalMult": global_multiplier,
-                                "clusterMult": board_mult,
+                                "clusterMult": 1,
                                 "winWithoutMult": sym_win,
                                 "overlay": {"reel": central_pos[0], "row": central_pos[1]},
                             },
                         }
-                    ]
-
+                    )
                     for positions in cluster:
                         board[positions[0]][positions[1]].explode = True
-                        if {
-                            "reel": positions[0],
-                            "row": positions[1],
-                        } not in exploding_symbols:
-                            exploding_symbols.append({"reel": positions[0], "row": positions[1]})
 
         return_data["totalWin"] += total_win
-
         return board, return_data
