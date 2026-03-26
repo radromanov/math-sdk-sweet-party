98ee22247b0fdcab171bb2504c1d94e951965954
r.romanov <r.romanov@wienergames.com>
2026-03-26 07:44:18 +0200

    refactor: rewrite game execs

--- games/sweet_party/game_executables.py
+++ games/sweet_party/game_executables.py
@@ -1,45 +1,22 @@
 from game_calculations import GameCalculations
 from src.calculations.cluster import Cluster
-from game_events import update_grid_mult_event
 from src.events.events import update_freespin_event
 
 
 class GameExecutables(GameCalculations):
     """Game dependent grouped functions."""
 
-    def reset_grid_mults(self):
-        """Initialize all grid position multipliers."""
-        self.position_multipliers = [
-            [0 for _ in range(self.config.num_rows[reel])] for reel in range(self.config.num_reels)
-        ]
-
-    def update_grid_mults(self):
-        """All positions start with 1x. If there is a win in that position, the grid point
-        is 'activated' and all subsequent wins on that position will double the grid value."""
-        if self.win_data["totalWin"] > 0:
-            for win in self.win_data["wins"]:
-                for pos in win["positions"]:
-                    if self.position_multipliers[pos["reel"]][pos["row"]] == 0:
-                        self.position_multipliers[pos["reel"]][pos["row"]] = 1
-                    else:
-                        self.position_multipliers[pos["reel"]][pos["row"]] += 1
-                        self.position_multipliers[pos["reel"]][pos["row"]] = min(
-                            self.position_multipliers[pos["reel"]][pos["row"]], self.config.maximum_board_mult
-                        )
-            update_grid_mult_event(self)
-
-    def get_clusters_update_wins(self):
+    def get_clusters_update_wins(self) -> None:
         """Find clusters on board and update win manager."""
-        clusters = Cluster.get_clusters(self.board, "wild")
-        return_data = {
+        clusters = Cluster.get_clusters(self.board)
+        return_data: dict = {
             "totalWin": 0,
             "wins": [],
         }
-        self.board, self.win_data = self.evaluate_clusters_with_grid(
+        self.board, self.win_data = self.evaluate_clusters_capped(
             config=self.config,
             board=self.board,
             clusters=clusters,
-            pos_mult_grid=self.position_multipliers,
             global_multiplier=self.global_multiplier,
             return_data=return_data,
         )
@@ -53,5 +30,4 @@ class GameExecutables(GameCalculations):
         self.fs += 1
         update_freespin_event(self)
         self.win_manager.reset_spin_win()
-        self.tumblewin_mult = 0
         self.win_data = {}
